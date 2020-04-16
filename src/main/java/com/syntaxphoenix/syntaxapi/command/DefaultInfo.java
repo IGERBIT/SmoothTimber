eda13a7ea3b6451d44a0ab643d86d815ab23793c
Laura Endreß <laura.endress@syntaxphoenix.com>
2020-04-16 15:08:49 +0200

    Switched to Maven Dependency

--- src/main/java/com/syntaxphoenix/syntaxapi/command/DefaultInfo.java
+++
@@ -1,9 +0,0 @@
-package com.syntaxphoenix.syntaxapi.command;
-
-public class DefaultInfo extends BaseInfo {
-	
-	public DefaultInfo(CommandManager manager, String label) {
-		super(manager, label);
-	}
-
-}
--- src/main/java/com/syntaxphoenix/syntaxapi/logging/color/ColorProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.syntaxphoenix.syntaxapi.logging.color;
-
-@FunctionalInterface
-public interface ColorProcessor {
-	
-	public String process(boolean stream, LogType type);
-	
-}
