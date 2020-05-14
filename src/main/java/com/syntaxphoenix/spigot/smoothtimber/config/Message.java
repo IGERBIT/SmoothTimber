9dc8dafd771366e92050f059c88a9d505a12fe30
Laura Endreß <laura.endress@syntaxphoenix.com>
2020-05-14 11:19:06 +0200

    Fixed a message

--- src/main/java/com/syntaxphoenix/spigot/smoothtimber/config/Message.java
+++ src/main/java/com/syntaxphoenix/spigot/smoothtimber/config/Message.java
@@ -37,7 +37,7 @@ public enum Message {
 	COMMAND_ONLY_PLAYER("&7Only a &5player &7can run this command!"), 
 	COMMAND_WIP("&7This command is work in progress!"), 
 	COMMAND_NON222EXISTENT("&7This command doesn't exist!"),
-	COMMAND_MISSING222PERMISSION("&7You're lacking the permission &5%permission &7to execute this command!"),
+	COMMAND_MISSING222PERMISSION("&7You're lacking the permission &5%permission% &7to execute this command!"),
 
 	/*
 	 * 
